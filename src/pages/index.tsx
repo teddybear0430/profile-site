55e9791f07f4ab8b27e1d4669bc522714348186b
teddybear0430 <teddybear0430@hotmail.com>
2021-03-29 01:13:18 +0900

    プロフィール文追加・SP時の文字の大きさ調整。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,13 +34,24 @@ const Home = ({ data }: PageProps<GatsbyTypes.HomeQuery>) => {
       <SEO title={title} description={description} author={author} />
       <h2 className="heading">{author}</h2>
       <Img className="w-32 rounded-full border border-gray-200 my-2" fluid={data.icon?.childImageSharp?.fluid!} />
-      <p>どちらかというとフロントの方が好きですが、5月からはバックエンドメインでやることになりそうです。</p>
-      <p>JamstackとVimが好きです。</p>
+      <div className="md:text-base text-sm">
+        <p>東京23区のどこかに住んでるウェブエンジニアです。</p>
+        <p>ウェブエンジニアになる前に、HTML・CSS・WordPress・jQueryを使ったサイトのコーディングと</p>
+        <p>デザイン（フォトショ・イラレ）をやっていた時期があったので、ウェブに携わってる期間自体は3年くらいあります。</p>
+        <br />
+        <p>デザインが苦手だったことや、なんとなくコーディングよりもウェブ開発の方が性に合ってそうと考えたのを機に、</p>
+        <p>2020年頃からウェブエンジニアとしてのキャリアをスタートしました。</p>
+        <br />
+        <p>どちらかというとフロントの方が好きですが、転職を機に2021年の5月からはバックエンドメインでやることになりそうです。</p>
+        <p>JamstackとVimが特に好きで、最近はCLIツール（tmux・Tig・rangerあたりがきてる）にも興味が出てきました。</p>
+      </div>
       <h2 className="heading">Career</h2>
-      <p>簡単なHTMLコーディング・サイトデザイン: 2018〜2019</p>
-      <p>ウェブ制作・ウェブ開発: 2020〜</p>
+      <div className="md:text-base text-sm">
+        <p>簡単なHTMLコーディング・サイトデザイン: 2018〜2020年3月頃まで</p>
+        <p>ウェブエンジニア: 2020年4月頃〜</p>
+      </div>
       <h2 className="heading">Skills</h2>
-      <div className="flex flex-wrap max-w-screen-sm">
+      <div className="flex flex-wrap max-w-screen-sm md:text-base text-sm">
         <span className="label">HTML</span>
         <span className="label">CSS・SCSS</span>
         <span className="label">JavaScript</span>
@@ -52,7 +63,7 @@ const Home = ({ data }: PageProps<GatsbyTypes.HomeQuery>) => {
         <span className="label">Docker</span>
       </div>
       <h2 className="heading">Twitter・GitHub・Blog</h2>
-      <ul className="ml-5 list-disc">
+      <ul className="ml-5 list-disc md:text-base text-sm">
         <li>
           <a className="hover:text-indigo-600" href="https://twitter.com/karukichi_yah">
             Twitter
