8eb5816e1bba96e803b729b50d7c76e696dba1fe
teddybear0430 <teddybear0430@hotmail.com>
2021-04-19 16:59:06 +0900

    文言修正とかコードのリファクタ。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,10 +29,37 @@ const Home = ({ data }: PageProps<GatsbyTypes.HomeQuery>) => {
   const description = data.site?.siteMetadata?.description;
   const author = data.site?.siteMetadata?.author;
 
+  const labels = [
+    'HTML',
+    'CSS・SCSS',
+    'JavaScript',
+    'TypeScript',
+    'React',
+    'PHP',
+    'Laravel',
+    'WordPress',
+    'Docker'
+  ];
+
+  const urls = [
+    {
+      name: 'Twitter',
+      url: 'https://twitter.com/karukichi_yah',
+    },
+    {
+      name: 'GitHub',
+      url: 'https://github.com/Yota-K',
+    },
+    {
+      name: 'カルキチのブログ',
+      url: 'https://karukichi-blog.netlify.app/',
+    }
+  ];
+
   return (
     <Layout>
       <SEO title={title} description={description} author={author} />
-      <h2 className="heading">{author}</h2>
+      <h2 className="heading">About</h2>
       <Img className="w-32 rounded-full border border-gray-200 my-2" fluid={data.icon?.childImageSharp?.fluid!} />
       <div className="md:text-base text-sm">
         <p>東京23区のどこかに住んでるウェブエンジニアです。</p>
@@ -40,6 +67,7 @@ const Home = ({ data }: PageProps<GatsbyTypes.HomeQuery>) => {
         <p>デザイン（フォトショ・イラレ）をやっていた時期があったので、ウェブに携わってる期間自体は3年くらいあります。</p>
         <br />
         <p>デザインが苦手だったことや、なんとなくコーディングよりもウェブ開発の方が性に合ってそうと考えたのを機に、</p>
+        <p>2019年の5月頃からプログラミングの学習を開始。</p>
         <p>2020年頃からウェブエンジニアとしてのキャリアをスタートしました。</p>
         <br />
         <p>どちらかというとフロントの方が好きですが、転職を機に2021年の5月からはバックエンドメインでやることになりそうです。</p>
@@ -48,37 +76,21 @@ const Home = ({ data }: PageProps<GatsbyTypes.HomeQuery>) => {
       <h2 className="heading">Career</h2>
       <div className="md:text-base text-sm">
         <p>簡単なHTMLコーディング・サイトデザイン: 2018〜2020年3月頃まで</p>
-        <p>ウェブエンジニア: 2020年4月頃〜</p>
+        <p>ウェブ制作系の企業でエンジニア: 2020年4月頃〜</p>
       </div>
       <h2 className="heading">Skills</h2>
       <div className="flex flex-wrap max-w-screen-sm md:text-base text-sm">
-        <span className="label">HTML</span>
-        <span className="label">CSS・SCSS</span>
-        <span className="label">JavaScript</span>
-        <span className="label">TypeScript</span>
-        <span className="label">React</span>
-        <span className="label">PHP</span>
-        <span className="label">Laravel</span>
-        <span className="label">WordPress</span>
-        <span className="label">Docker</span>
+        {labels.map((el , i) => (
+          <span key={i} className="label">{el}</span>
+        ))}
       </div>
       <h2 className="heading">Twitter・GitHub・Blog</h2>
       <ul className="ml-5 list-disc md:text-base text-sm">
-        <li>
-          <a className="hover:text-indigo-600" href="https://twitter.com/karukichi_yah">
-            Twitter
-          </a>
-        </li>
-        <li>
-          <a className="hover:text-indigo-600" href="https://github.com/Yota-K">
-            GitHub
-          </a>
-        </li>
-        <li>
-          <a className="hover:text-indigo-600" href="https://karukichi-blog.netlify.app/">
-            カルキチのブログ
-          </a>
-        </li>
+        {urls.map((el , i) => (
+          <li>
+            <a key={i} className="hover:text-indigo-600" href={el.url}>{el.name}</a>
+          </li>
+        ))}
       </ul>
     </Layout>
   );
